2a3aebb301747529ecd622a5ce53bd8560440b6e
IRCF <technique@ircf.fr>
2014-09-19 08:51:52 +0200

    added ircf skin

---
+++ public_html/plugins/ispconfig3_wblist/ispconfig3_account/config/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+$rcmail_config['identity_limit'] = false;
+$rcmail_config['remote_soap_user'] = 'roundcube';
+$rcmail_config['remote_soap_pass'] = '5YrDuw4v5i';
+$rcmail_config['soap_url'] = 'https://admin.ircf.fr:8080/remote/';
+?>
