2c978eb907d0ebb733d9446009aa95e2f63f5519
IRCF <technique@ircf.fr>
2014-09-27 15:22:30 +0200

    customized autologon plugin

--- plugins/autologon/autologon.php
+++ plugins/autologon/autologon.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Sample plugin to try out some hooks.
- * This performs an automatic login if accessed from localhost
- *
- * @license GNU GPLv3+
- * @author Thomas Bruederli
- */
 class autologon extends rcube_plugin
 {
   public $task = 'login';
@@ -19,30 +12,37 @@ class autologon extends rcube_plugin
 
   function startup($args)
   {
+    $rcmail = rcmail::get_instance();
     // change action to login
-    if (empty($_SESSION['user_id']) && !empty($_GET['_autologin']) && $this->is_localhost())
+    if (empty($_SESSION['user_id']) && !empty($_GET['_autologin']))
       $args['action'] = 'login';
-
     return $args;
   }
 
   function authenticate($args)
   {
-    if (!empty($_GET['_autologin']) && $this->is_localhost()) {
-      $args['user'] = 'me';
-      $args['pass'] = '******';
-      $args['host'] = 'localhost';
-      $args['cookiecheck'] = false;
-      $args['valid'] = true;
+    if (!empty($_GET['_autologin']) && !empty($_GET['uid']) && !empty($_GET['auth'])) {
+      $rcmail = rcmail::get_instance();
+      $db = $rcmail->get_dbh();
+      $result = $db->query("SELECT `email`,`pw` FROM `our_user_table` WHERE `id` = '{$_GET['uid']}'");
+      $data = $db->fetch_assoc($result);
+      if ( !empty($data) )
+      {
+        $email = $data['email'];
+        $pw = $data['pw'];
+        $date = date('Ymd');
+	// YYYYMMDD (no time since this will increase the likelihood of an authentication failure)
+        $expect = md5($date . $pw);
+        $auth = $_GET['auth'];
+        if ( $auth == $expect )
+        {
+          $args['user'] = $email;
+          $args['pass'] = $pw;
+          //$args['host'] = 'localhost';  // not sure why this was needed
+        }
+      }
     }
-
     return $args;
   }
 
-  function is_localhost()
-  {
-    return $_SERVER['REMOTE_ADDR'] == '::1' || $_SERVER['REMOTE_ADDR'] == '127.0.0.1';
-  }
-
 }
-
